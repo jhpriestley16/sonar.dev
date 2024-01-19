f821bdbd535d9d3efb28f4a29e90034773855288
jhpriestley16 <153106449+jhpriestley16@users.noreply.github.com>
2024-01-19 13:24:35 +0000

    local commit, music page open song menu

--- PointNemo/js/global.js
+++ PointNemo/js/global.js
@@ -74,7 +74,15 @@ function toggleAppearance() {
     }
 }
 
+function toggleSongExpand() {
+    let openMenu = document.querySelector('.music-player-expanded');
 
+    if (openMenu.style.visibility === 'hidden') {
+        openMenu.style.visibility = 'visible';
+    } else {
+        openMenu.style.visibility = 'hidden';
+    }
+}
 
 function openMobileSidebar() {
     let openSide = document.querySelector('.mobile-sidebar');

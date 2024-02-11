15711ee51ac3b2f685e10ab19a351503e4f3f376
jhpriestley16 <153106449+jhpriestley16@users.noreply.github.com>
2024-02-11 23:45:10 +0000

    experimenting with scripts

---
+++ PointNemo/js/filter-check.js
@@ -0,0 +1,10 @@
+/* function clearAllFilters() {
+    // Get references to the original and target containers
+    var originalContainer = document.getElementById('filtersContainer');
+    var targetContainer = document.getElementById('selectedFilters');
+    
+    // Move all children from the target container back to the original container
+    while (targetContainer.firstChild) {
+        originalContainer.appendChild(targetContainer.firstChild);
+    }
+} */

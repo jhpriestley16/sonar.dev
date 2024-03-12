d0f20e2d5c105ea846b14a378122d68fba97e759
jhpriestley16 <153106449+jhpriestley16@users.noreply.github.com>
2024-03-12 12:03:16 +0000

    local commit

---
+++ music/js/global.js
@@ -0,0 +1,114 @@
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+
+console.log(darkMode);
+
+//check if dark mode is enabled
+//if enabled we want to turn it off
+//if disabled turn it on
+const enableGreyMode = () => {
+    //add class darkmode to the body
+    document.body.classList.add('greymode');
+    //update darkmode in the local storage
+    localStorage.setItem('greyMode', 'enabled');
+}; 
+
+
+const enableDarkMode = () => {
+    //add class darkmode to the body
+    document.body.classList.add('darkmode');
+    //update darkmode in the local storage
+    localStorage.setItem('darkMode', 'enabled');
+};
+
+const disableDarkMode = () => {
+    //add class darkmode to the body
+    document.body.classList.remove('darkmode');
+    //update darkmode in the local storage
+    localStorage.setItem('darkMode', null);
+};
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+const googleSignIn = document.getElementsByClassName('g_id_signin');
+const gSignInDark = googleSignIn.getAttribute('data-theme');
+
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    
+    if (darkMode !== 'enabled') {
+        enableDarkMode();
+        console.log(darkMode);
+    } else {
+        disableDarkMode();
+        console.log(darkMode);
+    }
+});
+
+function toggleColorScheme() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    if (prefersDark) {
+        enableDarkMode();
+        // Add other styles for light mode
+    } else {
+        disableDarkMode();
+        // Add other styles for dark mode
+    }
+}
+
+
+function toggleMenu() {
+    let = document.querySelector('.toggled-menu');
+
+    if(let.style.visibility === 'hidden') {
+        let.style.visibility = 'visible';
+    } else {
+        let.style.visibility = 'hidden';
+    }
+}
+
+function toggleAppearance() {
+    let openMenu = document.getElementById('appearance-menu');
+
+    if (openMenu.style.visibility === 'hidden') {
+        openMenu.style.visibility = 'visible';
+    } else {
+        openMenu.style.visibility = 'hidden';
+    }
+}
+
+function toggleSongExpand() {
+    let openMenu = document.querySelector('.music-player-expanded');
+
+    if (openMenu.style.display === 'none') {
+        openMenu.style.display = 'block';
+    } else {
+        openMenu.style.display = 'none';
+    }
+}
+
+function openMobileSidebar() {
+    let openSide = document.querySelector('.mobile-sidebar');
+
+    if (openSide.style.display === 'none') {
+        openSide.style.display = 'block';
+    } else {
+        openSide.style.display = 'none';
+    }
+}
+
+function openSettings() {
+    let openMenu = document.querySelector('.mobile-settings-menu-opened');
+
+    if (openMenu.style.visibility === 'hidden') {
+        openMenu.style.visibility = 'visible';
+    } else {
+        openMenu.style.visibility = 'hidden';
+    }
+}
+
+
